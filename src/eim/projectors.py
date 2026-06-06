4e53d3adeba662b669f0d54079e7aded8100afbe
physicsbytom.com <tom@zeroonenet.com>
2026-06-06 17:50:25 -0400

    Repair projectors module

--- src/eim/projectors.py
+++ src/eim/projectors.py
@@ -10,3 +10,8 @@ def calculate_rho_proxies(node_data, G):
     rho3 = np.array([node_data[n]['local_load'] for n in nodes], dtype=float)
     rho3p = np.array([_scar(G, n) for n in nodes], dtype=float)
     rho5 = np.array([node_data[n]['saturation'] for n in nodes], dtype=float)
+    return rho3, rho3p, rho5
+
+
+def calculate_memory_seam_vectors_and_norms(G, node_data):
+   
