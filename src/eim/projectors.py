7408a600738b27c7745b1776a9fd83aeb3fdf5de
physicsbytom.com <tom@zeroonenet.com>
2026-06-06 17:42:46 -0400

    Repair projector proxy module compactly

--- src/eim/projectors.py
+++ src/eim/projectors.py
@@ -1,15 +1,16 @@
 import numpy as np
 
 
+def _scar(G, n):
+    return sum(G.edges[n, nb].get('scar_weight', 0.0) for nb in G.neighbors(n))
+
+
 def calculate_rho_proxies(node_data, G):
     nodes = list(G.nodes())
-    loads = np.array([node_data[n]['local_load'] for n in nodes], dtype=float)
-    scars = np.array([sum(G.edges[n, nb].get('scar_weight', 0.0) for nb in G.neighbors(n)) for n in nodes], dtype=float)
-    sats = np.array([node_data[n]['saturation'] for n in nodes], dtype=float)
-    return loads, scars, sats
+    rho3 = np.array([node_data[n]['local_load'] for n in nodes], dtype=float)
+    rho3p = np.array([_scar(G, n) for n in nodes], dtype=float)
+    rho5 = np.array([node_data[n]['saturation'] for n in nodes], dtype=float)
+    return rho3, rho3p, rho5
 
 
-def calculate_memory_seam_vectors_and_norms(G, node_data):
-    rho3, rho3p, _ = calculate_rho_proxies(node_data, G)
-    n3 = float(np.linalg.norm(rho3))
-    n3p
+def calculate_memory
