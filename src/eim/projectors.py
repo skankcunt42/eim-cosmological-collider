055092d7cc493c34d658209675b98c4dfd0a4188
physicsbytom.com <tom@zeroonenet.com>
2026-06-06 17:59:39 -0400

    Fix projectors module syntax

--- src/eim/projectors.py
+++ src/eim/projectors.py
@@ -2,11 +2,14 @@ import numpy as np
 
 
 def _scar(G, n):
-    return sum(G.edges[n, nb].get('scar_weight', 0.0) for nb in G.neighbors(n))
+    total = 0.0
+    for nb in G.neighbors(n):
+        total += G.edges[n, nb].get('scar_weight', 0.0)
+    return total
 
 
 def calculate_rho_proxies(node_data, G):
     nodes = list(G.nodes())
     rho3 = np.array([node_data[n]['local_load'] for n in nodes], dtype=float)
     rho3p = np.array([_scar(G, n) for n in nodes], dtype=float)
-    rho5 = np.array([node_data[n]['saturation'] for n in nodes], dtype=float
+    rho5 = np.array([node_data
