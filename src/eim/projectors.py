aa6b17c30aa4e7150a5350eb4de3d19f8ea54079
physicsbytom.com <tom@zeroonenet.com>
2026-06-06 17:44:45 -0400

    Repair projector proxy module completely

--- src/eim/projectors.py
+++ src/eim/projectors.py
@@ -1,16 +1,13 @@
 import numpy as np
 
+def _scar(G,n):
+    return sum(G.edges[n,nb].get('scar_weight',0.0) for nb in G.neighbors(n))
 
-def _scar(G, n):
-    return sum(G.edges[n, nb].get('scar_weight', 0.0) for nb in G.neighbors(n))
+def calculate_rho_proxies(node_data,G):
+    nodes=list(G.nodes())
+    a=np.array([node_data[n]['local_load'] for n in nodes],float)
+    b=np.array([_scar(G,n) for n in nodes],float)
+    c=np.array([node_data[n]['saturation'] for n in nodes],float)
+    return a,b,c
 
-
-def calculate_rho_proxies(node_data, G):
-    nodes = list(G.nodes())
-    rho3 = np.array([node_data[n]['local_load'] for n in nodes], dtype=float)
-    rho3p = np.array([_scar(G, n) for n in nodes], dtype=float)
-    rho5 = np.array([node_data[n]['saturation'] for n in nodes], dtype=float)
-    return rho3, rho3p, rho5
-
-
-def calculate_memory_seam
+def
