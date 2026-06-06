72af457287277a708b27d571245853ad0a2ca372
physicsbytom.com <tom@zeroonenet.com>
2026-06-06 16:05:42 -0400

    Update __init__.py

--- src/eim/__init__.py
+++ src/eim/__init__.py
@@ -1,4 +1,15 @@
-# EIM Simulation Package
+from .core import make_macro_dodec_cluster, initialize_observer_defect, calculate_local_load_and_capacity
+from .projectors import calculate_rho_proxies, calculate_memory_seam_vectors_and_norms, calculate_seam_asymmetry
+from .dynamics import (
+    update_evap_state, update_edge_weights, update_interaction_pressure,
+    obs_commit, move_observer_with_gravity, inject_gravitational_wave
+)
+from .simulation import run_tri_lobe_eim_simulation
+from .visualization import plot_diagnostics
 
-from .core import run_tri_lobe_eim_simulation
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+__all__ = [
+    'make_macro_dodec_cluster', 'initialize_observer_defect',
+    'calculate_rho_proxies', 'calculate_memory_seam_vectors_and_norms',
+    'run_tri_lobe_eim_simulation', 'plot_diagnostics'
+]
