7d8553eaf7b3161b5b735b89a35dc5e1a3a87cb4
physicsbytom.com <tom@zeroonenet.com>
2026-06-06 17:45:35 -0400

    Fix projector proxy syntax

--- src/eim/projectors.py
+++ src/eim/projectors.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def _scar(G,n):
-    return sum(G.edges[n,nb].get('scar_weight',0.0) for nb in G.neighbors(n))
 
-def calculate_rho_proxies(node_data,G):
-    nodes=list(G.nodes())
-    a=np.array([node_data[n]['local_load'] for n in nodes],float)
-    b=np.array([_scar(G,n) for n in nodes],float)
-    c=np.array([node_data[n]['saturation'] for n in nodes],float)
-    return a,b,c
+def _scar(G, n):
+    return sum(G.edges[n, nb].get('scar_weight', 0.0) for nb in G.neighbors(n))
 
-def
+
+def calculate_rho_proxies(node_data, G):
+    nodes = list(G.nodes())
+    rho3 = np.array([node_data[n]['local_load'] for n in nodes], float)
+    rho3p = np.array([_scar(G, n) for n in nodes], float)
+    rho5 = np.array([node_data[n]['saturation'] for n in nodes], float)
+    return rho3, rho3p, rho5
